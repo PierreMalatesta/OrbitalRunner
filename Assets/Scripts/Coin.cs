6a85b5846c752ff71137a3fcb395a9e7c1f9cce7
Pie Eip <Erreip2000@outlook.com>
2021-09-28 14:28:30 +0930

    Added scoring now i need to make it save the coins to use for the store

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Coin : MonoBehaviour
 {
     private float coin = 0;
 
+    public TextMeshProUGUI TextCoins;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "coin")
         {
+            coin++;
+            TextCoins.text = coin.ToString();
             Destroy(other.gameObject);
         }
     }
